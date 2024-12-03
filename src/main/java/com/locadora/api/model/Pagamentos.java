1fc7fc83d20d10915500d764f2fb5821980dbadc
Juliano <juliano.pinzan@summit-bra.com>
2024-12-03 11:33:07 -0300

    includ model Pagamentos

--- src/main/java/com/locadora/api/model/Pagamentos.java
+++ src/main/java/com/locadora/api/model/Pagamentos.java
@@ -1,8 +1,10 @@
 package com.locadora.api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -12,4 +14,24 @@ import lombok.*;
 @NoArgsConstructor
 @Table(name = "pagamentos")
 public class Pagamentos {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @OneToOne
+    @JoinColumn(name = "locacao_id", referencedColumnName = "id")
+    private Locacoes locacaoId;
+
+    @Column(name = "data_pagamento")
+    private LocalDateTime dataPagamento;
+
+    @Column(name = "valor_pago")
+    private Double valorPago;
+
+    @Column(name = "metodo_pagamento")
+    private String metodoPgt;
+
+    @Column(name = "status_pagamento")
+    private String statusPgt;
 }
