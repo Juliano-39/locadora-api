e858170f4f3f3e60290f96f53246422df1c63809
Juliano <juliano.pinzan@summit-bra.com>
2024-12-03 12:32:21 -0300

    id type adjust

--- src/main/java/com/locadora/api/repository/ClientesRepository.java
+++ src/main/java/com/locadora/api/repository/ClientesRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 @Repository
-public interface ClientesRepository extends JpaRepository<Clientes, UUID> {
+public interface ClientesRepository extends JpaRepository<Clientes, Long> {
 }
